1b62a9f711fcc98524e00c498ed2f4f3fe901d57
yuchen <yuchenz27@outlook.com>
2023-09-01 17:33:47 +0800

    add reference

--- Practice01_Weather/Practice01_Weather/Practice01_WeatherApp.swift
+++ Practice01_Weather/Practice01_Weather/Practice01_WeatherApp.swift
@@ -5,6 +5,9 @@
 //  Created by Yuchen Zhang on 2023/8/30.
 //
 
+// This project is inspired by Sean Allen's YouTube tutorial:
+// https://www.youtube.com/watch?v=HXoVSbwWUIk&list=PL8seg1JPkqgHyWCBHwXGmfysQpEQTfC3z&index=1
+
 import SwiftUI
 
 @main
