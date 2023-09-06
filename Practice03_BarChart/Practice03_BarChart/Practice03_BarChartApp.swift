0bce0febd0c6b1cc635484534496349dbcfbe8da
yuchen <yuchenz27@outlook.com>
2023-09-06 20:40:50 +0800

    add 03

---
+++ Practice03_BarChart/Practice03_BarChart/Practice03_BarChartApp.swift
@@ -0,0 +1,20 @@
+//
+//  Practice03_BarChartApp.swift
+//  Practice03_BarChart
+//
+//  Created by Yuchen Zhang on 2023/9/6.
+//
+
+// This project is inspired by Sean Allen's YouTube tutorial:
+// https://www.youtube.com/watch?v=4utsyqhnS4g&list=PL8seg1JPkqgHyWCBHwXGmfysQpEQTfC3z&index=4
+
+import SwiftUI
+
+@main
+struct Practice03_BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
